fc6d84d32656df997e84871afba913dd500645f8
SvettkaK <svettka09@gmail.com>
2016-11-08 05:23:38 +0400

    к 8.11.16

---
+++ Task01/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task01
+{
+    class Vector
+    {
+        public double x, y;//координаты вектора
+        public Vector(double Ax, double Ay, double Bx, double By)
+        {
+            //координаты вектора
+            x = Ax - Bx;
+            y = Ay - By;
+        }
+
+        public double Length()//длина вектора
+        {
+            return Math.Sqrt((x*x) + (y*y));//return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+        }
+
+        public double scalarProduct(Vector b)//скалярное произведение векторов
+        {
+            return (this.x * b.x) + (this.y * b.y);
+        }
+
+        public double AngleBetwen(Vector b)//угол между векторами
+        {
+            return Math.Acos(scalarProduct(b) / (Length() * b.Length()));
+        }
+    };
+
+
+    class Program
+    {
+        public string belongs(double x, double y, string g)
+        {
+            bool res = false;
+            
+            if (g == "а" || g == "f")
+            {
+                if (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1) res = true;
+            }
+
+            if (g == "б" || g == ",")
+            {
+                if (x * x + y * y <= 1 && x * x + y * y >= 0.5 * 0.5) res = true;
+            }
+
+            if (g == "в" || g == "d")
+            {
+                if (x <= 1 && x >= -1 && y <= 1 && y >= -1) res = true;
+            }
+ 
+            if (g == "г" || g == "u")
+            {
+                Vector a = new Vector(x, y, 0, 1);
+                Vector b = new Vector(x, y, 1, 0);
+                Vector c = new Vector(x, y, 0, -1);
+                Vector d = new Vector(x, y, -1, 0);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(d) + d.AngleBetwen(a)) * 180 / Math.PI;
+
+                if (sumAngle == 360 || sumAngle == 180) res = true;
+            }
+              
+            if (g == "д" || g == "l")
+            {
+                Vector a = new Vector(x, y, 0, 1);
+                Vector b = new Vector(x, y, 0.5, 0);
+                Vector c = new Vector(x, y, 0, -1);
+                Vector d = new Vector(x, y, -0.5, 0);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(d) + d.AngleBetwen(a)) * 180 / Math.PI;
+
+                if (sumAngle == 360 || sumAngle == 180) res = true;
+            }
+            
+            if (g == "е" || g == "t")
+            {
+                Vector a = new Vector(x, y, 0, 1);
+                Vector b = new Vector(x, y, -2, 0);
+                Vector c = new Vector(x, y, 0, -1);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(a)) * 180 / Math.PI;
+
+                if (sumAngle == 360 || sumAngle == 180 || (x * x + y * y <= 1 && x >= 0)) res = true;
+            }
+            
+            if (g == "ж" || g == ";")
+            {
+                Vector a = new Vector(x, y, 0, 2);
+                Vector b = new Vector(x, y, -1, -1);
+                Vector c = new Vector(x, y, 1, -1);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(a)) * 180 / Math.PI;
+
+                if (sumAngle == 360 || sumAngle == 180) res = true;
+            }
+
+            if (g == "з" || g == "p")
+            {
+                Vector a = new Vector(x, y, -1, 1);
+                Vector b = new Vector(x, y, 0, 0);
+                Vector c = new Vector(x, y, 1, 1);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(a)) * 180 / Math.PI;
+
+                if (x <= 1 && x >= -1 && y <= 0 && y >= -2 && (sumAngle != 360 || sumAngle != 180)) res = true;
+            }
+              
+            if (g == "и" || g == "b")
+            {
+                Vector a = new Vector(x, y, -1, 1);
+                Vector b = new Vector(x, y, -2, -1);
+                Vector c = new Vector(x, y, 1, 0);
+                Vector d = new Vector(x, y, 0, 0);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(a)) * 180 / Math.PI;
+                double sumAngle2 = Math.Abs(a.AngleBetwen(d) + d.AngleBetwen(c) + c.AngleBetwen(a)) * 180 / Math.PI;
+
+                if ((sumAngle == 360 || sumAngle == 180) && (sumAngle2 != 360 && sumAngle2 != 180)) res = true;
+            }
+
+            if (g == "к" || g == "r")
+            {
+                Vector a = new Vector(x, y, -1, 1);
+                Vector b = new Vector(x, y, 0, 0);
+                Vector c = new Vector(x, y, 1, 1);
+
+                double sumAngle = Math.Abs(a.AngleBetwen(b) + b.AngleBetwen(c) + c.AngleBetwen(a)) * 180 / Math.PI;
+
+                if (sumAngle == 360 || sumAngle == 180 || y >= 1) res = true;
+            }
+
+            return "Точка (" + x + ";" + y + ") " + (res == true ? "принадлежит" : "НЕ принадлежит") + " фигуре " + g;
+        }
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            
+            char ans = '+';
+            while (ans == '+')
+                try
+                {
+                    Console.WriteLine("Введите координаты точки:");
+                    Console.Write("x: ");
+                    double x = double.Parse(Console.ReadLine());
+                    Console.Write("y: ");
+                    double y = double.Parse(Console.ReadLine());
+
+                    Console.Write("Выберите фигуру(а-к): ");
+                    string graph = Console.ReadLine();
+
+                    Console.WriteLine(obj.belongs(x, y, graph));
+
+                    Console.WriteLine("\nПовторить(+/-)?");
+                    ans = char.Parse(Console.ReadLine());
+                    Console.WriteLine("\n\n");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Вы ввели не корректное значение. Повторите попытку...\n\n");
+                }
+        }
+
+
+    }
+}
+
---
+++ Task02/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ans = '+';
+            do
+                try
+                {
+                    Console.Write("Введите число h: ");
+                    double h = Double.Parse(Console.ReadLine());
+
+                    double a = Math.Sqrt((Math.Abs(Math.Sin(8 * h)) + 17) / Math.Pow(1 - Math.Sin(4 * h) * Math.Cos(h * h + 18), 2));
+                    double b = Math.Sqrt(3 / (3 + Math.Abs(Math.Tan(a * h * h) - Math.Sin(a * h))));
+                    double c = (a * h * h * Math.Sin(b * h)) + (b * h * h * h * Math.Cos(a * h));
+
+                    double D = b * b - 4 * a * c;
+                    Console.WriteLine("\na = {0:0.##}", a);
+                    Console.WriteLine("b = {0:0.##}", b);
+                    Console.WriteLine("c = {0:0.##}", c);
+                    Console.WriteLine("D = {0:0.##}\n", D);
+
+                    if (D < 0) Console.WriteLine("D<0 Уравнение не имеет корней");
+
+                    if (D == 0) Console.WriteLine("D=0 Уравнение имеет 2 одинаковых корня, X =" + (-b / (2 * a)));
+
+                    if (D > 0)
+                    {
+                        Console.Write("D>0 Уравнение имеет 2 различных корня: ");
+                        Console.Write("X1 = {0:0.##};", ((-b + Math.Sqrt(D)) / (2 * a)));
+                        Console.Write(" X2 = {0:0.##}\n", ((-b - Math.Sqrt(D)) / (2 * a)));
+                    }
+
+                    Console.WriteLine("\nПовторить(+/-)?");
+                    ans = char.Parse(Console.ReadLine());
+                    Console.WriteLine("\n");
+                }
+                catch
+                {
+                    Console.WriteLine("Введены недопустимые символы. Повторите попытку...\n");
+                    ans = '+';
+                }
+            while (ans == '+');
+
+
+                    
+        }
+    }
+}
